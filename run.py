89157c0b8ced4101caffd88e206ba31cdd015ce6
BogdanKuklenko <qoii1988@gmail.com>
2025-09-23 08:51:20 +1000

    Ап

--- run.py
+++ run.py
@@ -1,18 +1,7 @@
 # run.py
-from movie_lottery import create_app, db
+from movie_lottery import create_app
 
 app = create_app()
-
-# --- НАЧАЛО НОВОГО КОДА ---
-# Создаем специальную команду для принудительного создания таблиц
-@app.cli.command("create-tables")
-def create_tables():
-    """Создает все таблицы базы данных."""
-    with app.app_context():
-        print("Создание таблиц базы данных...")
-        db.create_all()
-        print("Таблицы успешно созданы.")
-# --- КОНЕЦ НОВОГО КОДА ---
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
